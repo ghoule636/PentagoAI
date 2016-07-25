b91389259011bce7b40e64ac44b10414dfba0103
ghoule636 <ghoule636@gmail.com>
2016-07-25 13:39:11 -0700

    Board now has toString() as well as functionality to add pieces.

--- Pentago.py
+++ Pentago.py
@@ -3,3 +3,17 @@
 # Programming Assignment 2
 # TCSS 435 AI Spring 2016
 """
+
+from Board import Board
+
+def main() :
+    test = Board()
+
+    print(test)
+
+    test.addPiece(3, 3, "w")
+    test.addPiece(1, 5, 'b')
+
+    print(test)
+
+main()
